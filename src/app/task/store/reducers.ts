f8a3fdd22be47f7639ca4219e8fba6aa5a874a6a
nibir <abu.shaikh.nibir@dohatec.com.bd>
2023-03-30 16:05:48 +0600

    issues solved...

---
+++ src/app/task/store/reducers.ts
@@ -0,0 +1,26 @@
+import * as TaskActions from "./actions";
+import { createReducer, on } from "@ngrx/store";
+import { TasksState } from "../tasts-state";
+
+// export const taskFeatureKey = 'taskStoreSelector';
+
+export const initialState: TasksState = {
+    isLoading: false,
+    tasks: [],
+    error: null,
+}
+
+export const tasksReducer = createReducer(
+    initialState,
+    on(TaskActions.getTasks, (state) => ({...state, isLoading: true})),
+    on(TaskActions.getTasksSuccess, (state, action) => ({
+        ...state,
+        isLoading: false,
+        tasks: action.tasks,
+    })),
+    on(TaskActions.getTasksFailure, (state, action) => ({
+        ...state,
+        isLoading: false,
+        error: action.error,
+    }))
+);
